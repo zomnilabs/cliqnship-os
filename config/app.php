c5c43523c0e874378caa5aca1f877a39a871e19e
Alleo Indong <freedom_war550@live.com.ph>
2017-02-18 22:49:03 +0800

    added laravel passport

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          */
         Laravel\Tinker\TinkerServiceProvider::class,
         Barryvdh\Cors\ServiceProvider::class,
+        Laravel\Passport\PassportServiceProvider::class,
 
         /*
          * Application Service Providers...
