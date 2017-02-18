2aa5a2a23f8b1278d8087cc47528724d0862278f
Alleo Indong <freedom_war550@live.com.ph>
2017-02-19 00:22:24 +0800

    added user addressbook

---
+++ database/migrations/2017_02_18_160420_create_user_addressbooks_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserAddressbooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_addressbooks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('identifier');
+            $table->enum('booking', ['booking', 'shipment']);
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('middle_name')->nullable();
+            $table->string('contact_number');
+            $table->string('email')->nullable();
+            $table->string('address_line_1');
+            $table->string('address_line_2');
+            $table->string('barangay');
+            $table->string('city');
+            $table->string('province');
+            $table->string('zip_cide');
+            $table->string('country');
+            $table->enum('address_type', ['office', 'residential']);
+            $table->string('landmarks');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_addressbooks');
+    }
+}
