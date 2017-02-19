c87a0440e4f6187f72a0d142d22a2e36072ab9fa
Nina Kristel Cheng <nkmcheng@live.com>
2017-02-19 23:59:12 +0800

    add card

--- resources/views/customers/dashboard.blade.php
+++ resources/views/customers/dashboard.blade.php
@@ -1,14 +1,69 @@
 @extends('layouts.app')
 
+@section('stylesheets')
+    <style>
+        .panel .panel-body{
+            font-size: 2em;
+        }
+    </style>
+@endsection
+
 @section('content')
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
+                <ol class="breadcrumb">
+                    <li class="active"> <i class="glyphicon glyphicon-home"></i> Dashboard</li>
+                </ol>
 
-                    <div class="panel-body">
-                        You are logged in!
+                <div class="row text-center">
+                    <div class="col-md-2">
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <i class="glyphicon glyphicon-tags"></i>
+                                <p><a href="#">New Products</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <i class="glyphicon glyphicon-tags"></i>
+                                <p>New Products</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <i class="glyphicon glyphicon-tags"></i>
+                                <p>New Products</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <i class="glyphicon glyphicon-tags"></i>
+                                <p>New Products</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <i class="glyphicon glyphicon-tags"></i>
+                                <p>New Products</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <i class="glyphicon glyphicon-tags"></i>
+                                <p>New Products</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
