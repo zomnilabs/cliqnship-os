20a0557b9e023527adf381004ef4b2647a84f575
Alleo Indong <freedom_war550@live.com.ph>
2017-02-25 07:41:34 +0800

    created booking model and migration

---
+++ database/migrations/2017_02_18_160449_create_bookings_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('booking_no');
+            $table->integer('user_id')->unsigned();
+            $table->integer('source_id')->unsigned();
+            $table->integer('addressbook_id')->unsigned();
+            $table->string('remarks');
+            $table->string('status');
+            $table->string('signature');
+            $table->date('pickup_date');
+            $table->integer('number_of_items');
+            $table->string('type_of_items');
+            $table->double('length');
+            $table->double('width');
+            $table->double('height');
+            $table->double('weight');
+            $table->timestamps();
+            $table->softDeletes();
+
+            // References
+            $table->foreign('user_id')
+                ->references('users')->on('id')
+                ->onDelete('cascade');
+
+            $table->foreign('addressbook_id')
+                ->references('addressbooks')->on('id')
+                ->onDelete('cascade');
+
+            $table->foreign('source_id')
+                ->references('sources')->on('id')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
