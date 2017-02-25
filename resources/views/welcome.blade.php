9fd566db7f68aeb0b80ea1b9452331e57dc4e469
Nina Kristel Cheng <nkmcheng@live.com>
2017-02-25 14:20:55 +0800

    update landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,17 +4,16 @@
 
     <!--- Main front -->
     <div id="main_front">
+        <div class="overlay"></div>
         <div class="front_txt">
             <div class="row">
-                <div class="col-md-6 col-md-offset-6 col-sm-6 col-sm-offset-6 col-xs-12">
+                <div class="col-md-6 col-sm-6 col-xs-12">
                     <p class="title"><strong>IS YOUR SHIPMENT READY?</strong></p>
                     <p class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <div class="small-title">Wanna have something shipped for your bussiness</div>
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse</p>
+                    <div class="small-title">Wanna have something shipped for your business</div>
                     <div class="front_signup">
                         <span>SIGN-UP HERE! AND HAVE IT SHIPPED</span>
                     </div>
