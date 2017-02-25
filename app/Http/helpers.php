6e772c55cd02a7949eefe5e44938b00803099903
Alleo Indong <freedom_war550@live.com.ph>
2017-02-25 09:23:51 +0800

    push ko

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -3,3 +3,12 @@
 function set_active($path, $active = 'active') {
     return call_user_func_array('Request::is', (array)$path) ? $active : '';
 }
+
+function number_of_bookings($userId, $status) {
+    if ($status === 'all') {
+        return \App\Models\Booking::where('user_id', $userId)->count();
+    }
+
+    return \App\Models\Booking::where('status', $status)
+        ->where('user_id', $userId)->count();
+}
