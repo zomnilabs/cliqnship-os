30ee5bda3ed73d3a1f5689f74c2bedf57df57208
Alleo Indong <freedom_war550@live.com.ph>
2017-02-25 11:07:02 +0800

    updated wizard

--- resources/views/customers/bookings/wizard.blade.php
+++ resources/views/customers/bookings/wizard.blade.php
@@ -7,15 +7,15 @@
                     <ul class="nav nav-tabs" role="tablist">
 
                         <li role="presentation" class="active">
-                            <a href="#step1" data-toggle="tab" aria-controls="step1" role="tab" title="Step 1">
+                            <a href="#step1" data-toggle="tab" aria-controls="step1" role="tab" title="Select address">
                             <span class="round-tab">
-                                <i class="glyphicon glyphicon-folder-open"></i>
+                                <i class="glyphicon glyphicon-globe"></i>
                             </span>
                             </a>
                         </li>
 
                         <li role="presentation" class="disabled">
-                            <a href="#step2" data-toggle="tab" aria-controls="step2" role="tab" title="Step 3">
+                            <a href="#step2" data-toggle="tab" aria-controls="step2" role="tab" title="Add Image">
                             <span class="round-tab">
                                 <i class="glyphicon glyphicon-picture"></i>
                             </span>
@@ -23,13 +23,28 @@
                         </li>
 
                         <li role="presentation" class="disabled">
-                            <a href="#step3" data-toggle="tab" aria-controls="step3" role="tab" title="Step 2">
+                            <a href="#step3" data-toggle="tab" aria-controls="step3" role="tab" title="Add more details">
                             <span class="round-tab">
                                 <i class="glyphicon glyphicon-pencil"></i>
                             </span>
                             </a>
                         </li>
 
+                        <li role="presentation" class="disabled">
+                            <a href="#step4" data-toggle="tab" aria-controls="step4" role="tab" title="Pouch Request">
+                            <span class="round-tab">
+                                <i class="glyphicon glyphicon-tag"></i>
+                            </span>
+                            </a>
+                        </li>
+
+                        <li role="presentation" class="disabled">
+                            <a href="#step5" data-toggle="tab" aria-controls="step5" role="tab" title="Booking Preview">
+                            <span class="round-tab">
+                                <i class="glyphicon glyphicon-eye-open"></i>
+                            </span>
+                            </a>
+                        </li>
 
                         <li role="presentation" class="disabled">
                             <a href="#complete" data-toggle="tab" aria-controls="complete" role="tab" title="Complete">
@@ -75,6 +90,7 @@
                                 <li><button type="button" class="btn btn-primary next-step">Next</button></li>
                             </ul>
                         </div>
+
                         <div class="tab-pane" role="tabpanel" id="step3">
                             <h3>Step 3</h3>
                             <p>This is step 3</p>
@@ -83,6 +99,26 @@
                                 <li><button type="button" class="btn btn-primary btn-info-full next-step">Save and Continue</button></li>
                             </ul>
                         </div>
+
+                        <div class="tab-pane" role="tabpanel" id="step4">
+                            <h3>Step 4</h3>
+                            <p>This is step 4</p>
+                            <ul class="list-inline pull-right">
+                                <li><button type="button" class="btn btn-default prev-step">Previous</button></li>
+                                <li><button type="button" class="btn btn-primary btn-info-full next-step">Save and Continue</button></li>
+                            </ul>
+                        </div>
+
+                        <div class="tab-pane" role="tabpanel" id="step5">
+                            <h3>Step 5</h3>
+                            <p>This is step 5</p>
+                            <ul class="list-inline pull-right">
+                                <li><button type="button" class="btn btn-default prev-step">Previous</button></li>
+                                <li><button type="button" class="btn btn-primary btn-info-full next-step">Save and Continue</button></li>
+                            </ul>
+                        </div>
+
+
                         <div class="tab-pane" role="tabpanel" id="complete">
                             <h3>Complete</h3>
                             <p>You have successfully completed all steps.</p>
