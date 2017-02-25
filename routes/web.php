22f49e0d38acca0bf46eac64c158a51c1e6878b0
markwaynepulido <mwpulido24@gmail.com>
2017-02-25 09:22:53 +0800

    Added Addressbooks controller and routes

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,7 @@ Route::group(['prefix' => 'customers', 'namespace' => 'Customers', 'middleware' 
     Route::group(['prefix' => 'bookings', 'namespace' => 'Bookings'], function() {
         Route::get('/', 'BookingsController@index');
     });
+    Route::group(['prefix' => 'addressbooks', 'namespace' => 'Addressbooks'], function() {
+        Route::get('/', 'AddressbooksController@index');
+    });
 });
