bbf2a4f305641d83bb025c02e609755e8a12acfd
markwaynepulido <mwpulido24@gmail.com>
2017-03-03 16:07:24 +0800

    Added relation to user on UserGroup model

--- app/Models/UserGroup.php
+++ app/Models/UserGroup.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -10,4 +11,9 @@ class UserGroup extends Model
     use SoftDeletes;
 
     protected $guarded = ['id'];
+
+    public function user()
+    {
+        return $this->hasMany(User::class);
+    }
 }
