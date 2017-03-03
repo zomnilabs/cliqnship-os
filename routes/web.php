ce961a7b770ac0e82060ce8037c7dedd76157896
markwaynepulido <mwpulido24@gmail.com>
2017-03-03 15:09:35 +0800

    Added routes for Admin

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,32 @@ Route::group(['prefix' => 'customers', 'namespace' => 'Customers', 'middleware' 
         Route::get('/cash-on-delivery', 'CodShipmentsController@index');
     });
 });
+
+Route::group(['prefix'=>'admin', 'namespace'=>'Admin'],function(){
+    Route::get('/', 'DashboardController@index');
+
+    Route::group(['prefix' => 'dispatching', 'namespace' => 'Dispatching'], function() {
+        Route::get('/', 'DispatchingController@index');
+    });
+    Route::group(['prefix' => 'riders', 'namespace' => 'Riders'], function() {
+        Route::get('/', 'RidersController@index');
+    });
+    Route::group(['prefix' => 'customers', 'namespace' => 'Customers'], function() {
+        Route::get('/', 'CustomersController@index');
+    });
+    Route::group(['prefix' => 'users', 'namespace' => 'Users'], function() {
+        Route::get('/', 'UsersController@index');
+    });
+    Route::group(['prefix' => 'bookings', 'namespace' => 'Bookings'], function() {
+        Route::get('/', 'BookingsController@index');
+    });
+    Route::group(['prefix' => 'shipments', 'namespace' => 'Shipments'], function() {
+        Route::get('/', 'ShipmentsController@index');
+    });
+    Route::group(['prefix' => 'reports', 'namespace' => 'Reports'], function() {
+        Route::get('/', 'ReportsController@index');
+    });
+    Route::group(['prefix' => 'cms', 'namespace' => 'CMS'], function() {
+        Route::get('/', 'CMSController@index');
+    });
+});
