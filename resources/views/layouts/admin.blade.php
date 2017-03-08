153b1db4920c8228a9b64108133a9aaf1e2e7124
markwaynepulido <mwpulido24@gmail.com>
2017-03-08 18:59:59 +0800

    Added ajax header on admin and app on layout views

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -104,6 +104,13 @@
 
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}"></script>
+<script>
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+</script>
 @yield('scripts')
 </body>
 </html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -111,6 +111,13 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+    <script>
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+    </script>
     @yield('scripts')
 </body>
 </html>
