a03a8f1a87dd8ed63716f50016648b1bece44f73
markwaynepulido <mwpulido24@gmail.com>
2017-03-09 01:37:16 +0800

    Created modal views for Updating and Saving addressbook on customer's views

--- resources/views/customers/addressbooks/modals/view.blade.php
+++ resources/views/customers/addressbooks/modals/view.blade.php
@@ -5,16 +5,16 @@
                 <span aria-hidden="true">&times;</span>
             </button>
             <div class="modal-header text-center">
-                <h4 class="modal-title" id="gridSystemModalLabel">Address Book</h4>
+                <h4 class="modal-title" id="modalTitle">Address Book</h4>
             </div>
-            <form method="PUT" id="viewForm" >
+            <form id="viewForm">
                 <div class="modal-body">
-                    {{ csrf_field() }}
+
                     <div class="row">
                         <div class="col-md-6">
                             <div class="form-group{{ $errors->has('identifier') ? ' has-error' : '' }}">
                                 <label for="Identifier">Identifier</label>
-                                <input type="text" class="form-control" name="identifier" id="identifier" value="" required/>
+                                <input type="text" class="form-control dataField" name="identifier" id="identifier"/>
                                 @if ($errors->has('identifier'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('identifier') }}</strong>
@@ -24,10 +24,10 @@
                         </div>
                         <div class="col-md-6">
                             <div class="form-group{{ $errors->has('type') ? ' has-error' : '' }}">
-                                <label for="Type">Type</label>
-                                <select name="type" class="form-control" id="type">
+                                <label for="type">Address Type</label>
+                                <select name="type" class="form-control dataField" id="type">
                                     <option value="booking">Booking</option>
-                                    <option value="shipmemt">Shipment</option>
+                                    <option value="shipment">Shipment</option>
                                 </select>
                                 @if ($errors->has('type'))
                                     <span class="help-block">
@@ -39,9 +39,9 @@
                     </div>
                     <div class="row">
                         <div class="col-md-4">
-                            <div class0.="form-group{{ $errors->has('first_name') ? ' has-error' : '' }}">
+                            <div class="form-group{{ $errors->has('first_name') ? ' has-error' : '' }}">
                                 <label for="first_name">First Name</label>
-                                <input type="text" class="form-control" name="first_name" id="first_name" required/>
+                                <input type="text" class="form-control dataField" name="first_name" id="first_name"/>
                                 @if ($errors->has('first_name'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('first_name') }}</strong>
@@ -52,7 +52,7 @@
                         <div class="col-md-4">
                             <div class="form-group{{ $errors->has('last_name') ? ' has-error' : '' }}">
                                 <label for="last_name">Last Name</label>
-                                <input type="text" class="form-control" name="last_name" id="last_name" required/>
+                                <input type="text" class="form-control dataField" name="last_name" id="last_name"/>
                                 @if ($errors->has('last_name'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('last_name') }}</strong>
@@ -63,7 +63,7 @@
                         <div class="col-md-4">
                             <div class="form-group{{ $errors->has('middle_name') ? ' has-error' : '' }}">
                                 <label for="middle_name">Middle Name</label>
-                                <input type="text" class="form-control" name="middle_name" id="middle_name" required/>
+                                <input type="text" class="form-control dataField" name="middle_name" id="middle_name"/>
                                 @if ($errors->has('middle_name'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('middle_name') }}</strong>
@@ -76,7 +76,7 @@
                         <div class="col-md-6">
                             <div class="form-group{{ $errors->has('contact_number') ? ' has-error' : '' }}">
                                 <label for="contact_number">Contact Number</label>
-                                <input type="text" class="form-control" name="contact_number" id="contact_number" required/>
+                                <input type="text" class="form-control dataField" name="contact_number" id="contact_number"/>
                                 @if ($errors->has('contact_number'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('contact_number') }}</strong>
@@ -87,7 +87,7 @@
                         <div class="col-md-6">
                             <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                                 <label for="email">Email Address</label>
-                                <input type="email" class="form-control" name="email" id="email" required/>
+                                <input type="email" class="form-control dataField" name="email" id="email"/>
                                 @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
@@ -100,7 +100,7 @@
                         <div class="col-md-12">
                             <div class="form-group{{ $errors->has('address_line_1') ? ' has-error' : '' }}">
                                 <label for="address_line_1">Address Line 1</label>
-                                <input type="text" class="form-control" name="address_line_1" id="address_line_1" required/>
+                                <input type="text" class="form-control dataField" name="address_line_1" id="address_line_1"/>
                                 @if ($errors->has('address_line_1'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('address_line_1') }}</strong>
@@ -113,7 +113,7 @@
                         <div class="col-md-12">
                             <div class="form-group{{ $errors->has('address_line_2') ? ' has-error' : '' }}">
                                 <label for="address_line_2">Address Line 2</label>
-                                <input type="text" class="form-control" name="address_line_2" id="address_line_2"/>
+                                <input type="text" class="form-control dataField" name="address_line_2" id="address_line_2"/>
                                 @if ($errors->has('address_line_2'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('address_line_2') }}</strong>
@@ -126,7 +126,7 @@
                         <div class="col-md-6">
                             <div class="form-group{{ $errors->has('barangay') ? ' has-error' : '' }}">
                                 <label for="barangay">Barangay</label>
-                                <input type="text" class="form-control" name="barangay" id="barangay" required/>
+                                <input type="text" class="form-control dataField" name="barangay" id="barangay"/>
                                 @if ($errors->has('barangay'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('barangay') }}</strong>
@@ -137,7 +137,7 @@
                         <div class="col-md-3">
                             <div class="form-group{{ $errors->has('city') ? ' has-error' : '' }}">
                                 <label for="city">City</label>
-                                <input type="text" class="form-control" name="city" id="city" required/>
+                                <input type="text" class="form-control dataField" name="city" id="city"/>
                                 @if ($errors->has('city'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('city') }}</strong>
@@ -148,7 +148,7 @@
                         <div class="col-md-3">
                             <div class="form-group{{ $errors->has('province') ? ' has-error' : '' }}">
                                 <label for="province">Province</label>
-                                <input type="text" class="form-control" name="province" id="province" required/>
+                                <input type="text" class="form-control dataField" name="province" id="province"/>
                                 @if ($errors->has('province'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('province') }}</strong>
@@ -161,7 +161,7 @@
                         <div class="col-md-6">
                             <div class="form-group{{ $errors->has('country') ? ' has-error' : '' }}">
                                 <label for="country">Country</label>
-                                <input type="text" class="form-control" name="country" id="country" required/>
+                                <input type="text" class="form-control dataField" name="country" id="country"/>
                                 @if ($errors->has('country'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('country') }}</strong>
@@ -172,7 +172,7 @@
                         <div class="col-md-3">
                             <div class="form-group{{ $errors->has('zip_code') ? ' has-error' : '' }}">
                                 <label for="zip_code">Zipcode</label>
-                                <input type="text" class="form-control" name="zip_code" id="zip_code" required/>
+                                <input type="text" class="form-control dataField" name="zip_code" id="zip_code"/>
                                 @if ($errors->has('zip_code'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('zip_code') }}</strong>
@@ -183,7 +183,7 @@
                         <div class="col-md-3">
                             <div class="form-group{{ $errors->has('landmarks') ? ' has-error' : '' }}">
                                 <label for="landmarks">Landmarks</label>
-                                <input type="text" class="form-control" name="landmarks" id="landmarks" required/>
+                                <input type="text" class="form-control dataField" name="landmarks" id="landmarks"/>
                                 @if ($errors->has('landmarks'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('landmarks') }}</strong>
@@ -196,7 +196,7 @@
                         <div class="col-md-6">
                             <div class="form-group{{ $errors->has('address_type') ? ' has-error' : '' }}">
                                 <label for="address_type">Address Type</label>
-                                <select name="address_type" class="form-control" id="address_type">
+                                <select name="address_type" class="form-control dataField" id="address_type">
                                     <option value="residential">Resedential</option>
                                     <option value="office">Office</option>
                                 </select>
@@ -216,12 +216,11 @@
                         </div>
                         <div class="col-md-3"></div>
                     </div>
-                    <input type="hidden" id="addressbookId" name="user_id" value="0"/>
                 </div>
 
                 <div class="modal-footer">
                     <button class="btn btn-default" type="button" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
-                    <button class="btn btn-success" type="submit"><i class="fa fa-floppy-o"></i> Save Address book</button>
+                    <button type="button" class="btn btn-success" id="formSubmit" onclick="storeData.call(this)"><i class="fa fa-floppy-o"></i> Update Addressbook</button>
                 </div>
             </form>
         </div>
