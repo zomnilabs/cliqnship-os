959832e8f7a8c6e01eb650f6cfa87495b493879b
markwaynepulido <mwpulido24@gmail.com>
2017-03-10 02:28:06 +0800

    Added store, update and delete methods on Admin RIdersController

--- app/Http/Controllers/Admin/Riders/RidersController.php
+++ app/Http/Controllers/Admin/Riders/RidersController.php
@@ -2,11 +2,68 @@
 namespace App\Http\Controllers\Admin\Riders;
 
 use App\Http\Controllers\Controller;
+use App\User;
+use App\Http\Requests\Admin\Riders\UpdateRiderRequest;
+use App\Http\Requests\Admin\Riders\StoreRiderRequest;
 
 class RidersController extends Controller
 {
     public function index()
     {
-        return view('admin.riders.index');
+        $riders = User::with('profile')
+            ->where('user_group_id', 4)
+            ->get();
+
+        return view('admin.riders.index', compact('riders'));
+    }
+
+    public function store(StoreRiderRequest $request )
+    {
+        $input = $request->all();
+        $user = [
+            'account_id'=> $input['account_id'],
+            'user_group_id'=> 4,
+            'email'=>$input['email'],
+            'password' => encrypt($input['password'])
+        ];
+
+        $profile = [
+            'first_name'=> $input['first_name'],
+            'last_name'=> $input['last_name'],
+            'middle_name'=>$input['middle_name'],
+            'company_name' => $input['company_name'],
+            'gender'=> $input['gender'],
+            'birthdate' => $input['birthdate']
+        ];
+        $users = User::create($user);
+        $users->profile()->create($profile);
+
+    }
+
+    public function update(UpdateRiderRequest $request,User $riderId)
+    {
+        $input = $request->all();
+        $user = [
+            'account_id'=> $input['account_id'],
+            'email'=>$input['email'],
+            'password' => encrypt($input['password'])
+        ];
+
+        $profile = [
+            'first_name'  => $input['first_name'],
+            'last_name'   => $input['last_name'],
+            'middle_name' => $input['middle_name'],
+            'company_name'=> $input['company_name'],
+            'gender'      => $input['gender'],
+            'birthdate'   => $input['birthdate']
+        ];
+
+        $riderId->update($user);
+        $riderId->profile()->update($profile);
+    }
+
+    public function destroy(User $riderId)
+    {
+        $riderId->delete($riderId);
     }
 }
