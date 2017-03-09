a907b928a6cd7e1db2f79a9adda5cd33f0b98e3f
markwaynepulido <mwpulido24@gmail.com>
2017-03-10 01:19:28 +0800

    Added UpdatedRiderRequest on Request for Admin Rider

---
+++ app/Http/Requests/Admin/Riders/UpdateRiderRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\Admin\Riders;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRiderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email'      => 'required|email|max:56|unique:users,email,' .$this->getSegmentFromEnd().',id',
+            'first_name' =>'required',
+            'middle_name'=>'required',
+            'last_name'  => 'required',
+            'birthdate'  => 'required'
+        ];
+    }
+
+    private function getSegmentFromEnd($position_from_end = 1) {
+        $segments =$this->segments();
+        return $segments[sizeof($segments) - $position_from_end];
+    }
+}
