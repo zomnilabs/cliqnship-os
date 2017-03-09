71e150c1aa6160855be4b8c5ebad6d0217ff3d58
markwaynepulido <mwpulido24@gmail.com>
2017-03-10 01:19:00 +0800

    Added StoreRiderRequest on Request for Admin Rider

---
+++ app/Http/Requests/Admin/Riders/StoreRiderRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Admin\Riders;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRiderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|email|max:56|unique',
+            'first_name' =>'required',
+            'middle_name'=>'required',
+            'last_name'  => 'required',
+            'birthdate'  => 'required'
+        ];
+    }
+}
