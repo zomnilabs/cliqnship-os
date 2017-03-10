d4e2d48e89178fe0fc41c33a5d2e75f9e86ab7c3
markwaynepulido <mwpulido24@gmail.com>
2017-03-10 22:36:36 +0800

    Added show, store, update and delete method on Admin CustomersController

--- app/Http/Controllers/Admin/Customers/CustomersController.php
+++ app/Http/Controllers/Admin/Customers/CustomersController.php
@@ -2,11 +2,68 @@
 namespace App\Http\Controllers\Admin\Customers;
 
 use App\Http\Controllers\Controller;
+use App\User;
+use App\Http\Requests\Admin\Customers\StoreCustomerRequest;
+use App\Http\Requests\Admin\Customers\UpdateCustomerRequest;
 
 class CustomersController extends Controller
 {
     public function index()
     {
-        return view('admin.customers.index');
+        $customers = User::with('profile','booking','addressbook')
+            ->where('user_group_id', 5)
+            ->get();
+
+        return view('admin.customers.index', compact('customers'));
+    }
+
+    public function store(StoreCustomerRequest $request )
+    {
+        $input = $request->all();
+        $user = [
+            'account_id'=> $input['account_id'],
+            'user_group_id'=> 5,
+            'email'=>$input['email'],
+            'password' => encrypt($input['password'])
+        ];
+
+        $profile = [
+            'first_name'=> $input['first_name'],
+            'last_name'=> $input['last_name'],
+            'middle_name'=>$input['middle_name'],
+            'company_name' => $input['company_name'],
+            'gender'=> $input['gender'],
+            'birthdate' => $input['birthdate']
+        ];
+        $users = User::create($user);
+        $users->profile()->create($profile);
+
+    }
+
+    public function update(UpdateCustomerRequest $request,User $customerId)
+    {
+        $input = $request->all();
+        $user = [
+            'account_id'=> $input['account_id'],
+            'email'=>$input['email'],
+            'password' => bcrypt($input['password'])
+        ];
+
+        $profile = [
+            'first_name'  => $input['first_name'],
+            'last_name'   => $input['last_name'],
+            'middle_name' => $input['middle_name'],
+            'company_name'=> $input['company_name'],
+            'gender'      => $input['gender'],
+            'birthdate'   => $input['birthdate']
+        ];
+
+        $customerId->update($user);
+        $customerId->profile()->update($profile);
+    }
+
+    public function destroy(User $customerId)
+    {
+        $customerId->delete();
     }
 }
