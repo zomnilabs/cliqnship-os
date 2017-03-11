07833a15a7590aa551a85c8288ffe66dc1eb1804
markwaynepulido <mwpulido24@gmail.com>
2017-03-11 12:49:27 +0800

    Added modal views on admin user on views

---
+++ resources/views/admin/users/modals/view.blade.php
@@ -0,0 +1,137 @@
+<div class="modal fade" id="viewUserModal" role="dialog" aria-labelledby="myLargeModalLabel">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content mcontent">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            <div class="modal-header text-center">
+                <h4 class="modal-title" id="modalTitle">Create Users</h4>
+            </div>
+            <form id="viewForm">
+                <div class="modal-body">
+                    {{csrf_field()}}
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="form-group{{ $errors->has('first_name') ? ' has-error' : '' }}">
+                                <label for="first_name">First Name</label>
+                                <input type="text" class="form-control dataField" name="first_name" id="first_name"/>
+                                @if ($errors->has('first_name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('first_name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group{{ $errors->has('last_name') ? ' has-error' : '' }}">
+                                <label for="last_name">Last Name</label>
+                                <input type="text" class="form-control dataField" name="last_name" id="last_name"/>
+                                @if ($errors->has('last_name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('last_name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group{{ $errors->has('middle_name') ? ' has-error' : '' }}">
+                                <label for="middle_name">Middle Name</label>
+                                <input type="text" class="form-control dataField" name="middle_name" id="middle_name"/>
+                                @if ($errors->has('middle_name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('middle_name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="form-group{{ $errors->has('user_group_id') ? ' has-error' : '' }}">
+                                <label for="user_group_id">Address Type</label>
+                                <select name="user_group_id" class="form-control dataField" id="user_group_id">
+                                    <option value="5">Customer</option>
+                                    <option value="4">Rider</option>
+                                    <option value="3">Dispatcher</option>
+                                    <option value="2">Staff</option>
+                                    <option value="1">Admin</option>
+                                </select>
+                                @if ($errors->has('user_group_id'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('user_group_id') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group{{ $errors->has('gender') ? ' has-error' : '' }}">
+                                <label for="gender">Gender</label>
+                                <select name="gender" class="form-control dataField" id="gender">
+                                    <option value="male">Male</option>
+                                    <option value="female">Female</option>
+                                </select>
+                                @if ($errors->has('gender'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('gender') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group{{ $errors->has('birthdate') ? ' has-error' : '' }}">
+                                <label for="birthdate">Birthday</label>
+                                <input type="date" class="form-control dataField" name="birthdate" id="birthdate"/>
+                                @if ($errors->has('birthdate'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('birthdate') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <label for="email">Email</label>
+                                <input type="email" class="form-control dataField" name="email" id="email"/>
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                <label for="password">Password</label>
+                                <input type="password" class="form-control dataField" name="password" id="password"/>
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group{{ $errors->has('company_name') ? ' has-error' : '' }}">
+                                <label for="company_name">Company Name</label>
+                                <input type="text" class="form-control dataField" name="company_name" id="contact_number"/>
+                                @if ($errors->has('company_name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('company_name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-default" type="button" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+                    <button type="button" class="btn btn-success" id="formSubmit" onclick="storeData.call(this)"><i class="fa fa-floppy-o"></i> Update Users</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
