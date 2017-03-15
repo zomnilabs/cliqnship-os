8eefcc7d64e9ceefb1e6d1bc73a9ce012ad5e266
Alleo Indong <freedom_war550@live.com.ph>
2017-03-15 13:33:29 +0800

    updated wizard form and create booking table

--- resources/assets/js/components/customer/booking/wizard-form.vue
+++ resources/assets/js/components/customer/booking/wizard-form.vue
@@ -31,13 +31,13 @@
                                 </a>
                             </li>
 
-                            <li role="presentation" class="disabled">
-                                <a href="#step4" data-toggle="tab" aria-controls="step4" role="tab" title="Pouch Request">
-                            <span class="round-tab">
-                                <i class="glyphicon glyphicon-tag"></i>
-                            </span>
-                                </a>
-                            </li>
+                            <!--<li role="presentation" class="disabled">-->
+                                <!--<a href="#step4" data-toggle="tab" aria-controls="step4" role="tab" title="Pouch Request">-->
+                            <!--<span class="round-tab">-->
+                                <!--<i class="glyphicon glyphicon-tag"></i>-->
+                            <!--</span>-->
+                                <!--</a>-->
+                            <!--</li>-->
 
                             <li role="presentation" class="disabled">
                                 <a href="#step5" data-toggle="tab" aria-controls="step5" role="tab" title="Booking Preview">
@@ -148,7 +148,7 @@
                                     </div>
                                     <div class="col-md-12">
                                         <div class="form-group">
-                                            <label for="pickup_date">Pickup Date</label>
+                                            <label for="pickup_date">Pickup Date <span class="text-danger">*</span></label>
                                             <input type="date" name="pickup_date"
                                                    @input="inputChange" v-bind:value="pickup_date"
                                                    id="pickup_date" class="form-control">
@@ -171,62 +171,62 @@
                                 </ul>
                             </div>
 
-                            <div class="tab-pane" role="tabpanel" id="step4">
-                                <h3>Step 4</h3>
-                                <p>Would you like to request a pouch?</p>
+                            <!--<div class="tab-pane" role="tabpanel" id="step4">-->
+                                <!--<h3>Step 4</h3>-->
+                                <!--<p>Would you like to request a pouch?</p>-->
 
-                                <div class="row">
-                                    <div class="col-md-5">
-                                        <div class="form-group">
-                                            <label for="pouch_size">Pouch Size</label>
-                                            <select name="pouch_size" id="pouch_size" class="form-control">
-                                                <option value="1">Small</option>
-                                                <option value="2">Medium</option>
-                                                <option value="3">Large</option>
-                                                <option value="4">Extra Large</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-5">
-                                        <div class="form-group">
-                                            <label for="quantity">Quantity</label>
-                                            <input type="number" name="quantity"
-                                                   @input="inputChange" v-bind:value="quantity"
-                                                   id="quantity" class="form-control">
-                                        </div>
-                                    </div>
-                                    <div class="col-md-2">
-                                        <button class="btn btn-success" type="button" style="margin-top: 26px;"><i class="fa fa-plus"></i> Add</button>
-                                    </div>
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <label for="remarks">Pouch Requested</label>
-                                            <table class="table table-hover">
-                                                <thead>
-                                                <tr>
-                                                    <th>Pouch Size</th>
-                                                    <th>Quantity</th>
-                                                </tr>
-                                                </thead>
-                                                <tbody>
-                                                <tr>
-                                                    <td>Medium</td>
-                                                    <td>2</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Large</td>
-                                                    <td>10</td>
-                                                </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
-                                </div>
-                                <ul class="list-inline pull-right">
-                                    <li><button type="button" class="btn btn-default prev-step" @click="prevTab">Previous</button></li>
-                                    <li><button type="button" class="btn btn-primary btn-info-full next-step" @click="nextTab">Next</button></li>
-                                </ul>
-                            </div>
+                                <!--<div class="row">-->
+                                    <!--<div class="col-md-5">-->
+                                        <!--<div class="form-group">-->
+                                            <!--<label for="pouch_size">Pouch Size</label>-->
+                                            <!--<select name="pouch_size" id="pouch_size" class="form-control">-->
+                                                <!--<option value="1">Small</option>-->
+                                                <!--<option value="2">Medium</option>-->
+                                                <!--<option value="3">Large</option>-->
+                                                <!--<option value="4">Extra Large</option>-->
+                                            <!--</select>-->
+                                        <!--</div>-->
+                                    <!--</div>-->
+                                    <!--<div class="col-md-5">-->
+                                        <!--<div class="form-group">-->
+                                            <!--<label for="quantity">Quantity</label>-->
+                                            <!--<input type="number" name="quantity"-->
+                                                   <!--@input="inputChange" v-bind:value="quantity"-->
+                                                   <!--id="quantity" class="form-control">-->
+                                        <!--</div>-->
+                                    <!--</div>-->
+                                    <!--<div class="col-md-2">-->
+                                        <!--<button class="btn btn-success" type="button" style="margin-top: 26px;"><i class="fa fa-plus"></i> Add</button>-->
+                                    <!--</div>-->
+                                    <!--<div class="col-md-12">-->
+                                        <!--<div class="form-group">-->
+                                            <!--<label for="remarks">Pouch Requested</label>-->
+                                            <!--<table class="table table-hover">-->
+                                                <!--<thead>-->
+                                                <!--<tr>-->
+                                                    <!--<th>Pouch Size</th>-->
+                                                    <!--<th>Quantity</th>-->
+                                                <!--</tr>-->
+                                                <!--</thead>-->
+                                                <!--<tbody>-->
+                                                <!--<tr>-->
+                                                    <!--<td>Medium</td>-->
+                                                    <!--<td>2</td>-->
+                                                <!--</tr>-->
+                                                <!--<tr>-->
+                                                    <!--<td>Large</td>-->
+                                                    <!--<td>10</td>-->
+                                                <!--</tr>-->
+                                                <!--</tbody>-->
+                                            <!--</table>-->
+                                        <!--</div>-->
+                                    <!--</div>-->
+                                <!--</div>-->
+                                <!--<ul class="list-inline pull-right">-->
+                                    <!--<li><button type="button" class="btn btn-default prev-step" @click="prevTab">Previous</button></li>-->
+                                    <!--<li><button type="button" class="btn btn-primary btn-info-full next-step" @click="nextTab">Next</button></li>-->
+                                <!--</ul>-->
+                            <!--</div>-->
 
                             <div class="tab-pane" role="tabpanel" id="step5">
                                 <h3>Step 5</h3>
@@ -275,13 +275,13 @@
                                         <p>{{ remarks }}</p>
                                     </div>
 
-                                    <div class="col-md-12">
-                                        <label>Pouch Request:</label>
-                                        <ul>
-                                            <li>2x Medium</li>
-                                            <li>10x Large</li>
-                                        </ul>
-                                    </div>
+                                    <!--<div class="col-md-12">-->
+                                        <!--<label>Pouch Request:</label>-->
+                                        <!--<ul>-->
+                                            <!--<li>2x Medium</li>-->
+                                            <!--<li>10x Large</li>-->
+                                        <!--</ul>-->
+                                    <!--</div>-->
                                 </div>
 
                                 <ul class="list-inline pull-right">
@@ -321,6 +321,7 @@
             return {
                 showError: false,
                 errorMessage: '',
+                tabPage: 1,
                 user_addressbook_id: '',
                 addressbooks: [],
                 addressbookOptions: [],
@@ -355,6 +356,7 @@
                 this.pickup_date = ''
                 this.remarks = ''
                 this.quantity = ''
+                this.tabPage = 1
             },
             readURL(input) {
 
@@ -394,16 +396,26 @@
                     return false
                 }
 
+                if (! this.pickup_date && this.tabPage >= 3) {
+                    this.showError = true;
+                    this.errorMessage = 'Please select a pickup date';
+
+                    return false
+                }
+
                 let $active = $('.wizard .nav-tabs li.active');
                 $active.next().removeClass('disabled');
                 $($active).next().find('a[data-toggle="tab"]').click();
 
                 this.showError = false;
                 this.errorMessage = '';
+                this.tabPage++;
             },
             prevTab() {
                 let $active = $('.wizard .nav-tabs li.active');
                 $($active).prev().find('a[data-toggle="tab"]').click();
+
+                this.tabPage--;
             },
             getAddress() {
                 axios.get(`/api/v1/address-books/${this.user_id}`).then(response => {
