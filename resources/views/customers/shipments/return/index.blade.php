e87c59a6d8c2bd136ecd558f11b6d073713249af
Nina Kristel Cheng <nkmcheng@live.com>
2017-03-15 12:56:28 +0800

    create layouts for auth

--- resources/views/customers/shipments/return/index.blade.php
+++ resources/views/customers/shipments/return/index.blade.php
@@ -1,5 +1,53 @@
 @extends('layouts.app')
 
+@section('scripts')
+    <script>
+        (function() {
+            $('#shippingTable tfoot tr.searchable td').each( function () {
+                let title = $(this).text();
+                if (title) {
+                    switch (title) {
+                        case 'Pickup Date':
+                            $(this).html( '<input type="date" class="form-control filter" style="width: 100%" placeholder="Search '+title+'" />' );
+
+                            break;
+                        case 'Status':
+                            let selectHTML = '<select class="form-control filter" style="width: 100%">';
+                            selectHTML += '<option value="">Filter Status</option>';
+                            selectHTML += '<option value="pending">Pending</option>';
+                            selectHTML += '<option value="accepted">Accepted</option>';
+                            selectHTML += '<option value="completed">Completed</option>';
+                            selectHTML += '<option value="rejected">Rejected</option>';
+                            selectHTML += '</select>';
+
+                            $(this).html(selectHTML);
+                            break;
+                        default:
+                            $(this).html( '<input class="form-control filter" type="text" style="width: 100%" placeholder="Search '+title+'" />' );
+                    }
+
+                }
+            });
+
+            let table = $('#shippingTable').DataTable();
+
+            // Apply the search
+            table.columns().every( function () {
+                let that = this;
+                $( '.filter', this.footer() ).on( 'keyup change', function () {
+                    if ( that.search() !== this.value ) {
+                        console.log(that.data());
+                        that
+                            .search( this.value )
+                            .draw();
+                    }
+                } );
+            } );
+        }())
+    </script>
+@endsection
+
+
 @section('content')
     <div class="header-info container-fluid">
         <div class="row">
@@ -39,10 +87,47 @@
             <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-body">
+                        <div class="hide">
+                            <div class="filter-categories">
+                                <div class="btn-group" data-toggle="buttons">
+                                    <label class="btn btn-sm btn-default active">
+                                        <input type="radio" name="filter" id="option1" value="all" autocomplete="off" checked> [{{ number_of_bookings(Auth::user()->id, 'all') }}] All
+                                    </label>
+                                    <label class="btn btn-sm btn-warning">
+                                        <input type="radio" name="filter" id="option2" value="pending" autocomplete="off"> [{{ number_of_bookings(Auth::user()->id, 'pending') }}] Pending
+                                    </label>
+                                    <label class="btn btn-sm btn-primary">
+                                        <input type="radio" name="filter" id="option3" value="accepted" autocomplete="off"> [{{ number_of_bookings(Auth::user()->id, 'accepted') }}] Accepted
+                                    </label>
 
-                        <table class="table table-bordered">
+                                    <label class="btn btn-sm btn-success">
+                                        <input type="radio" name="filter" id="option3" value="completed" autocomplete="off"> [{{ number_of_bookings(Auth::user()->id, 'completed') }}] Completed
+                                    </label>
+
+                                    <label class="btn btn-sm btn-danger">
+                                        <input type="radio" name="filter" id="option3" value="rejected" autocomplete="off"> [{{ number_of_bookings(Auth::user()->id, 'rejected') }}] Rejected
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <table class="table table-bordered" id="shippingTable">
+                            <tfoot class="filter-footer">
+                            <tr class="searchable">
+                                <td class="hide">Id #</td>
+                                <td>Booking #</td>
+                                <td>Pickup Date</td>
+                                <td>Pickup Address</td>
+                                <td># of Items</td>
+                                <td>Remarks</td>
+                                <td>Status</td>
+                                <td></td>
+                            </tr>
+                            </tfoot>
+
                             <thead>
                             <tr>
+                                <th class="hide">Id #</th>
                                 <th>Booking #</th>
                                 <th>Pickup Date</th>
                                 <th>Pickup Address</th>
@@ -54,44 +139,7 @@
                             </thead>
 
                             <tbody>
-                            <tr>
-                                <td>KJLKJLJL</td>
-                                <td>February 25, 2017</td>
-                                <td>29 Anonas Quezon City</td>
-                                <th>1</th>
-                                <th>Pending</th>
-                                <th>This is a remarks</th>
-                                <th>
-                                    <button class="btn btn-danger"><i class="glyphicon glyphicon-trash"></i></button>
-                                    <button class="btn btn-default"><i class="glyphicon glyphicon-eye-open"></i></button>
-                                </th>
-                            </tr>
 
-                            <tr>
-                                <td>KJLKJLJL</td>
-                                <td>February 25, 2017</td>
-                                <td>29 Anonas Quezon City</td>
-                                <th>1</th>
-                                <th>Pending</th>
-                                <th>This is a remarks</th>
-                                <th>
-                                    <button class="btn btn-danger"><i class="glyphicon glyphicon-trash"></i></button>
-                                    <button class="btn btn-default"><i class="glyphicon glyphicon-eye-open"></i></button>
-                                </th>
-                            </tr>
-
-                            <tr>
-                                <td>KJLKJLJL</td>
-                                <td>February 25, 2017</td>
-                                <td>29 Anonas Quezon City</td>
-                                <th>1</th>
-                                <th>Pending</th>
-                                <th>This is a remarks</th>
-                                <th>
-                                    <button class="btn btn-danger"><i class="glyphicon glyphicon-trash"></i></button>
-                                    <button class="btn btn-default"><i class="glyphicon glyphicon-eye-open"></i></button>
-                                </th>
-                            </tr>
                             </tbody>
                         </table>
                     </div>
---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'CliqNShip') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/landing.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/font-awesome/css/font-awesome.min.css') }}">
+    @yield('stylesheets')
+
+    <!-- Scripts -->
+    <script>
+        window.Laravel = {!! json_encode([
+            'csrfToken' => csrf_token(),
+        ]) !!};
+    </script>
+</head>
+<body>
+<div id="{{ !Auth::guest() ? 'app' : 'guest' }}">
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    <img src="images/logo.png"/>
+                </a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav">
+                    @if (! Auth::guest())
+                        @if (Auth::user()->user_group_id == 1)
+                            <li class="{{ set_active(['admin']) }}"><a href="/admin">Dashboard <span class="sr-only">(current)</span></a></li>
+                            <li class="{{ set_active(['admin/dispatching*']) }}"><a href="/admin/dispatching">Dispatching</a></li>
+                            <li class="{{ set_active(['admin/riders*']) }}"><a href="/admin/riders">Riders</a></li>
+                            <li class="{{ set_active(['admin/customers*']) }}"><a href="/admin/customers">Customers</a></li>
+                            <li class="{{ set_active(['admin/users*']) }}"><a href="/admin/users">Users</a></li>
+                            <li class="{{ set_active(['admin/bookings*']) }}"><a href="/admin/bookings">Bookings</a></li>
+                            <li class="{{ set_active(['admin/shipments*']) }}"><a href="/admin/shipments">Shipments</a></li>
+                            <li class="{{ set_active(['admin/reports*']) }}"><a href="/admin/reports">Reports</a></li>
+                            <li class="{{ set_active(['admin/cms*']) }}"><a href="/admin/cms">CMS</a></li>
+                        @else
+                            <li class="{{ set_active(['customers']) }}"><a href="/customers">Dashboard <span class="sr-only">(current)</span></a></li>
+                            <li class="{{ set_active(['customers/addressbooks*']) }}"><a href="/customers/addressbooks">Addressbooks</a></li>
+                            <li class="{{ set_active(['customers/bookings*']) }}"><a href="/customers/bookings">Bookings</a></li>
+                            <li class="{{ set_active(['customers/shipments*']) }} dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown"
+                                   role="button" aria-haspopup="true" aria-expanded="false">
+                                    Shipments <span class="caret"></span></a>
+
+                                <ul class="dropdown-menu">
+                                    <li><a href="/customers/shipments">All Shipments</a></li>
+                                    <li><a href="/customers/shipments/returned">Returned Shipments</a></li>
+                                    <li role="separator" class="divider"></li>
+                                    <li><a href="/customers/shipments/cash-on-delivery">Cash-on-Delivery Shipments</a></li>
+                                </ul>
+                            </li>
+                        @endif
+                    @endif
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ route('login') }}">Login</a></li>
+                        <li><a href="{{ route('register') }}">Register</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->profile->full_name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#">Profile</a></li>
+
+                                <li role="separator" class="divider"></li>
+                                <li>
+                                    <a href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="content" style="margin-top: 5%">
+        @yield('content')
+    </div>
+
+<!-- Fix for vue -->
+    @if(Auth::guest())
+        <div id="app"></div>
+    @endif
+</div>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    @yield('scripts')
+</body>
+</html>
