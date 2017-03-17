f34f217ee3ec7cc60221336e3031357c420339df
Alleo Indong <freedom_war550@live.com.ph>
2017-03-18 03:57:21 +0800

    fix user accounts model

--- app/Http/Controllers/Auth/FacebookAuthController.php
+++ app/Http/Controllers/Auth/FacebookAuthController.php
@@ -3,7 +3,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\User;
-use App\UserAccount;
+use App\Models\UserAccount;
 
 class FacebookAuthController extends Controller {
     public function redirectToProvider()
