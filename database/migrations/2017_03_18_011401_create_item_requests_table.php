f0a4575b140336611795cb2bba35f8a24702f67d
Alleo Indong <freedom_war550@live.com.ph>
2017-03-18 09:18:57 +0800

    added item request model and schema

---
+++ database/migrations/2017_03_18_011401_create_item_requests_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_requests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('user_addressbook_id');
+            $table->enum('size', ['small', 'medium', 'large']);
+            $table->integer('quantity');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_requests');
+    }
+}
