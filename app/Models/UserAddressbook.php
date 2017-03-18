de757510bde47a1c82930dd6d588692ae3040e2f
Nina Kristel Cheng <nkmcheng@live.com>
2017-03-18 15:33:34 +0800

    populate item request in admin

--- app/Models/UserAddressbook.php
+++ app/Models/UserAddressbook.php
@@ -2,18 +2,23 @@
 
 namespace App\Models;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class UserAddressbook extends Model
 {
     use SoftDeletes;
-    
+
     protected $guarded = ['id'];
 
     public function bookings()
     {
         return $this->hasMany(Booking::class);
+    }
+    public function items()
+    {
+        return $this->hasMany(ItemRequest::class);
     }
 
     public function shipments()
@@ -25,4 +30,10 @@ class UserAddressbook extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function getFullAddress()
+    {
+        return ucwords($this->attributes['identifier'] . ': ' . $this->attributes['address_line_1'] . ', ' . $this->attributes['barangay'] . ', ' . $this->attributes['city']);
+    }
+
 }
--- resources/views/admin/dispatching/item-requests.blade.php
+++ resources/views/admin/dispatching/item-requests.blade.php
@@ -7,14 +7,30 @@
 @section('scripts')
     <script>
         (function() {
-            $('.table thead tr.searchable th').each( function () {
-                var title = $(this).text();
+            $('.table tfoot tr.searchable td').each( function () {
+                let title = $(this).text();
                 if (title) {
-                    $(this).html( '<input type="text" style="width: 100%" placeholder="Search '+title+'" />' );
+                    switch (title) {
+                        default:
+                            $(this).html( '<input class="form-control filter" type="text" style="width: 100%" placeholder="Search '+title+'" />' );
+                    }
                 }
             });
 
-            $('.table').dataTable();
+            let table = $('.table').DataTable();
+
+            // Apply the search
+            table.columns().every( function () {
+                let that = this;
+                $( '.filter', this.footer() ).on( 'keyup change', function () {
+                    if ( that.search() !== this.value ) {
+                        console.log(that.data());
+                        that
+                            .search( this.value )
+                            .draw();
+                    }
+                } );
+            } );
         }())
     </script>
 @endsection
@@ -26,26 +42,13 @@
                 <div class="breadcrumbs">
                     <ol class="breadcrumb">
                         <li><a href="/admin">Dashboard</a></li>
-                        <li class="active">Dispatching</li>
+                        <li class="active">Item Request Dispatching</li>
                     </ol>
                 </div>
                 <div class="header-title pull-left">
-                    <h1>Dispatching</h1>
+                    <h1>Item Request Dispatching</h1>
                 </div>
 
-                <div class="page-actions pull-right">
-                    <button class="btn btn-primary" data-toggle="modal" data-target="#createShippingModal">
-                        <i class="fa fa-plus"></i>
-                        Import Shipments Assignment</button>
-
-                    <button class="btn btn-primary">
-                        <i class="fa fa-upload"></i>
-                        Import Bookings Assignment</button>
-
-                    <button class="btn btn-primary">
-                        <i class="fa fa-download"></i>
-                        Import Returned Shipments</button>
-                </div>
             </div>
         </div>
     </div>
@@ -56,175 +59,54 @@
                 <div class="box box-primary">
                     <div class="box-header">
                         <div class="box-title pull-left">
-                            <h1>Bookings</h1>
+                            <h1>Item Requests</h1>
                         </div>
                         <div class="page-actions pull-right">
                             <button class="btn btn-primary">
                                 <i class="fa fa-plus"></i>
-                                Add booking Assignment</button>
+                                Add Item Request Assignment</button>
                         </div>
                     </div>
                     <div class="box-body">
                         <table class="table table-bordered">
+                            <tfoot class="filter-footer">
+                            <tr class="searchable">
+                                <td>Id #</td>
+                                <td>Address</td>
+                                <td>Size</td>
+                                <td>Quantity</td>
+                                <td></td>
+                            </tr>
+                            </tfoot>
                             <thead>
                             <tr>
                                 <th>Id #</th>
-                                <th>Name</th>
-                                <th>Type</th>
-                                <th>Address Type</th>
                                 <th>Address</th>
-                                <th>Contact #</th>
-                                <th>Email Address</th>
+                                <th>Size</th>
+                                <th>Quantity</th>
                                 <th>Actions</th>
                             </tr>
                             </thead>
 
                             <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>Edward Lim</td>
-                                <td>Booking</td>
-                                <td>Residential</td>
-                                <td>San-miguel Tarlac</td>
-                                <td>09123456789</td>
-                                <td>edward@gmail.com</td>
-                                <td>
-                                    <button class="btn btn-danger"><i class="fa fa-trash"></i></button>
-                                    <button class="btn btn-default"><i class="fa fa-edit"></i></button>
-                                </td>
-                            </tr>
-                            {{--@foreach($addressbooks as $addressbook)--}}
-                            {{--<tr id="addressbook-{{$addressbook->id}}">--}}
-                            {{--<td>{{$addressbook->id}}</td>--}}
-                            {{--<td>{{$addressbook->last_name}} {{$addressbook->middle_name}} {{$addressbook->last_name}}</td>--}}
-                            {{--<td>{{ ucwords($addressbook->type) }}</td>--}}
-                            {{--<td>{{ ucwords($addressbook->address_type) }}</td>--}}
-                            {{--<th>{{$addressbook->address_line_1}}</th>--}}
-                            {{--<th>{{$addressbook->contact_number}}</th>--}}
-                            {{--<th>{{$addressbook->email}}</th>--}}
-                            {{--<th>--}}
-                            {{--<button class="btn btn-danger"><i class="fa fa-trash"></i></button>--}}
-                            {{--<button class="btn btn-default"><i class="fa fa-edit"></i></button>--}}
-                            {{--</th>--}}
-                            {{--</tr>--}}
-                            {{--@endforeach--}}
+                            @foreach($itemRequests as $itemRequest)
+                                <tr>
+                                    <td>{{ $itemRequest->id }}</td>
+                                    <td>{{ $itemRequest->address->getFullAddress() }}</td>
+                                    <td>{{ $itemRequest->size }}</td>
+                                    <td>{{ $itemRequest->quantity }}</td>
+                                    <td>
+                                        <button class="btn btn-danger"><i class="fa fa-trash"></i></button>
+                                        <button class="btn btn-default"><i class="fa fa-edit"></i></button>
+                                    </td>
+                                </tr>
+                            @endforeach
                             </tbody>
                         </table>
                     </div>
                 </div>
             </div>
-
-            <div class="col-md-12">
-                <div class="box box-primary">
-                    <div class="box-header">
-                        <div class="box-title pull-left">
-                            <h1>Shipment</h1>
-                        </div>
-                        <div class="page-actions pull-right">
-                            <button class="btn btn-primary">
-                                <i class="fa fa-plus"></i>
-                                Add shipment Assignment</button>
-                        </div>
-                    </div>
-                    <div class="box-body">
-                        <table class="table table-bordered">
-                            <thead>
-                            <tr>
-                                <th>Id #</th>
-                                <th>Name</th>
-                                <th>Type</th>
-                                <th>Address Type</th>
-                                <th>Address</th>
-                                <th>Contact #</th>
-                                <th>Email Address</th>
-                                <th>Actions</th>
-                            </tr>
-                            </thead>
-
-                            <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>Edward Lim</td>
-                                <td>Booking</td>
-                                <td>Residential</td>
-                                <td>San-miguel Tarlac</td>
-                                <td>09123456789</td>
-                                <td>edward@gmail.com</td>
-                                <td>
-                                    <button class="btn btn-danger"><i class="fa fa-trash"></i></button>
-                                    <button class="btn btn-default"><i class="fa fa-edit"></i></button>
-                                </td>
-                            </tr>
-                            {{--@foreach($addressbooks as $addressbook)--}}
-                            {{--<tr id="addressbook-{{$addressbook->id}}">--}}
-                            {{--<td>{{$addressbook->id}}</td>--}}
-                            {{--<td>{{$addressbook->last_name}} {{$addressbook->middle_name}} {{$addressbook->last_name}}</td>--}}
-                            {{--<td>{{ ucwords($addressbook->type) }}</td>--}}
-                            {{--<td>{{ ucwords($addressbook->address_type) }}</td>--}}
-                            {{--<th>{{$addressbook->address_line_1}}</th>--}}
-                            {{--<th>{{$addressbook->contact_number}}</th>--}}
-                            {{--<th>{{$addressbook->email}}</th>--}}
-                            {{--<th>--}}
-                            {{--<button class="btn btn-danger"><i class="fa fa-trash"></i></button>--}}
-                            {{--<button class="btn btn-default"><i class="fa fa-edit"></i></button>--}}
-                            {{--</th>--}}
-                            {{--</tr>--}}
-                            {{--@endforeach--}}
-
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-
         </div>
     </div>
-    <div class="container-fluid">
-        <div class="box box-primary">
-            <div class="box-header">
-                <div class="box-title pull-left">
-                    <h1>Returned Shipments</h1>
-                </div>
-                <div class="page-actions pull-right">
-                    <button class="btn btn-danger">
-                        <i class="fa fa-plus"></i>
-                        Add Returned Shipment</button>
-                </div>
-            </div>
-            <div class="box-body">
-                <table class="table table-bordered">
-                    <thead>
-                    <tr>
-                        <th>Shipment #</th>
-                        <th>Actions</th>
-                        <th>Date</th>
-                        <th>Rider</th>
-                        <th>Reason</th>
-                        <th>Returned Times</th>
-                        <th>Returned To</th>
-                        <th>Actions</th>
-                    </tr>
-                    </thead>
 
-                    <tbody>
-                    {{--@foreach($addressbooks as $addressbook)--}}
-                    {{--<tr id="addressbook-{{$addressbook->id}}">--}}
-                    {{--<td>{{$addressbook->id}}</td>--}}
-                    {{--<td>{{$addressbook->last_name}} {{$addressbook->middle_name}} {{$addressbook->last_name}}</td>--}}
-                    {{--<td>{{ ucwords($addressbook->type) }}</td>--}}
-                    {{--<td>{{ ucwords($addressbook->address_type) }}</td>--}}
-                    {{--<th>{{$addressbook->address_line_1}}</th>--}}
-                    {{--<th>{{$addressbook->contact_number}}</th>--}}
-                    {{--<th>{{$addressbook->email}}</th>--}}
-                    {{--<th>--}}
-                    {{--<button class="btn btn-danger"><i class="fa fa-trash"></i></button>--}}
-                    {{--<button class="btn btn-default"><i class="fa fa-edit"></i></button>--}}
-                    {{--</th>--}}
-                    {{--</tr>--}}
-                    {{--@endforeach--}}
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
 @endsection
