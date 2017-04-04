81c5cb6440954be0c8c0a5ea2479119bc3fe3ec7
Nina Kristel Cheng <nkmcheng@live.com>
2017-04-04 15:35:34 +0800

    dashboard link

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -66,28 +66,13 @@
                         <li><a href="{{ route('login') }}">Login</a></li>
                         <li><a href="{{ route('register') }}">Register</a></li>
                     @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->profile->full_name }} <span class="caret"></span>
-                            </a>
+                        @if (Auth::user()->userGroup->slug === 'customer')
+                            <li><a href="/customers">Dashboard</a></li>
+                        @endif
 
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="#">Profile</a></li>
-
-                                <li role="separator" class="divider"></li>
-                                <li>
-                                    <a href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        Logout
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        {{ csrf_field() }}
-                                    </form>
-                                </li>
-                            </ul>
-                        </li>
+                        @if (Auth::user()->userGroup->slug === 'admin')
+                            <li><a href="/admin">Dashboard</a></li>
+                        @endif
                     @endif
                 </ul>
             </div>
