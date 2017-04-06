58587e1dface1ada8b879e929385e5f0c7f140b6
Alleo Indong <freedom_war550@live.com.ph>
2017-04-07 04:23:55 +0800

    list riders on the receiving

--- app/Http/Controllers/Admin/Receiving/ShipmentsController.php
+++ app/Http/Controllers/Admin/Receiving/ShipmentsController.php
@@ -3,11 +3,36 @@ namespace App\Http\Controllers\Admin\Receiving;
 
 use App\Http\Controllers\Controller;
 use App\Models\Shipment;
+use App\Models\ShipmentAssignment;
+use App\User;
+use Carbon\Carbon;
 
 class ShipmentsController extends Controller {
     public function all()
     {
-        return view('admin.receiving.shipment');
+        $riders = User::with('profile', 'shipmentAssignments')
+            ->where('user_group_id', 4)
+            ->get();
+
+        $today = Carbon::today('Asia/Manila')->toDateString();
+        $result = [];
+        foreach ($riders as $rider) {
+            $pendingAssignmentCount = ShipmentAssignment::where('user_id', $rider->id)
+                ->where('status', 'pending')
+                ->count();
+
+            $completedShipmentToday = ShipmentAssignment::where('status', 'completed')
+                ->whereDate('created_at', '=', $today)
+                ->count();
+
+            $rider['pending'] = $pendingAssignmentCount;
+            $rider['completed_today'] = $completedShipmentToday;
+
+            $result[] = $rider;
+        }
+
+        return view('admin.receiving.shipment')
+            ->with('riders', $result);
     }
 
     public function remit()
