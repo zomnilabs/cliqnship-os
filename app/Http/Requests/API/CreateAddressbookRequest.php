d543a5ab30b4012c5c958ad6dfd942529b8360e1
Alleo Indong <freedom_war550@live.com.ph>
2017-04-24 11:51:17 +0800

    created addressbook create request

---
+++ app/Http/Requests/API/CreateAddressbookRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests\API;
+
+class CreateAddressbookRequest extends AbstractAPIRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'identifier'    => 'required',
+            'type'          => 'in:booking,shipment',
+            'first_name'    => 'required',
+            'last_name'     => 'required',
+            'contact_number'    => 'required',
+            'email'             => 'email',
+            'address_line_1'    => 'required',
+            'city'              => 'required',
+            'province'          => 'required',
+            'zip_code'          => 'required',
+            'address_type'      => 'in:office,residential'
+        ];
+    }
+}
