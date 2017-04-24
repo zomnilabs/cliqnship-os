df4eeda467fc43340fb4aa16fd9bd8cac2ac60f2
Alleo Indong <freedom_war550@live.com.ph>
2017-04-24 11:50:33 +0800

    updated abstract api controller, added transformers

--- app/Http/Controllers/Api/AbstractAPIController.php
+++ app/Http/Controllers/Api/AbstractAPIController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Transformers\Transformerable;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use League\Fractal\Manager;
+use League\Fractal\Pagination\IlluminatePaginatorAdapter;
+use League\Fractal\Resource\Collection;
+use League\Fractal\Resource\Item;
 
 abstract class AbstractAPIController extends BaseController
 {
@@ -19,6 +23,13 @@ abstract class AbstractAPIController extends BaseController
         $this->fractal = $fractal;
     }
 
+    /**
+     * Get all filter data from request
+     *
+     * @param $request
+     * @param $filterables
+     * @return mixed
+     */
     protected function getFilters($request, $filterables)
     {
         $filters = $request->all();
@@ -28,6 +39,13 @@ abstract class AbstractAPIController extends BaseController
         return $request->only($filters);
     }
 
+    /**
+     * Filter query
+     *
+     * @param $query
+     * @param $filters
+     * @return mixed
+     */
     protected function filter($query, $filters)
     {
         if (! count($filters)) {
@@ -39,5 +57,35 @@ abstract class AbstractAPIController extends BaseController
         }
 
         return $query->paginate(10);
+    }
+
+    /**
+     * Transform a collection
+     *
+     * @param $collection
+     * @param Transformerable $transformer
+     * @return \League\Fractal\Scope
+     */
+    protected function transformCollection($collection, Transformerable $transformer)
+    {
+        $data = $collection->getCollection();
+        $resource = new Collection($data, $transformer);
+        $resource->setPaginator(new IlluminatePaginatorAdapter($collection));
+
+        return $this->fractal->createData($resource);
+    }
+
+    /**
+     * Transform an item
+     *
+     * @param $item
+     * @param Transformerable $transformer
+     * @return \League\Fractal\Scope
+     */
+    protected function transformItem($item, Transformerable $transformer)
+    {
+        $resource = new Item($item, $transformer);
+
+        return $this->fractal->createData($resource);
     }
 }
