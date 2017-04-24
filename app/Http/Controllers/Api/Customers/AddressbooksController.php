c0d5cd8a8268477bea141369173c96d6549da5f4
Alleo Indong <freedom_war550@live.com.ph>
2017-04-24 11:50:44 +0800

    updated addressbookcontroller

--- app/Http/Controllers/Api/Customers/AddressbooksController.php
+++ app/Http/Controllers/Api/Customers/AddressbooksController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers\Api\Customers;
 
 use App\Http\Controllers\Api\AbstractAPIController;
+use App\Http\Requests\API\CreateAddressbookRequest;
 use App\Models\UserAddressbook;
 use App\Traits\ApiResponse;
 use App\Transformers\Customers\AddressbookTransformer;
@@ -35,10 +36,7 @@ class AddressbooksController extends AbstractAPIController {
         $paginator = $this->filter($model, $filters);
 
         // Transform Result
-        $addresses = $paginator->getCollection();
-        $resource = new Collection($addresses, new AddressbookTransformer);
-        $resource->setPaginator(new IlluminatePaginatorAdapter($paginator));
-        $result = $this->fractal->createData($resource);
+        $result = $this->transformCollection($paginator, new AddressbookTransformer);
 
         // Return response
         return $this->responseOk($result->toArray());
@@ -61,14 +59,30 @@ class AddressbooksController extends AbstractAPIController {
         }
 
         // Transform Result
-        $resource = new Item($address, new AddressbookTransformer);
-        $result = $this->fractal->createData($resource);
+        $result = $this->transformItem($address, new AddressbookTransformer);
 
+        // Return response
         return $this->responseOk($result->toArray());
     }
 
-    public function store($userId)
+    public function store(CreateAddressbookRequest $request, $userId)
     {
+        // Check user
+        if ($request->user()->id !== (int) $userId) {
+            return $this->responseUnauthorized();
+        }
 
+        $input = $request->all();
+
+        $address = UserAddressbook::create($input);
+        if (! $address) {
+            return $this->responseBadRequest(['something went wrong when creating a new addressbook']);
+        }
+
+        // Transform Result
+        $result = $this->transformItem($address, new AddressbookTransformer);
+
+        // Return response
+        return $this->responseCreated($result);
     }
 }
