5f73af63680eb1344d293235d46ac40ba4e0b0f1
Alleo Indong <freedom_war550@live.com.ph>
2017-05-02 01:41:24 +0800

    import on customer side

--- app/Http/Controllers/Customers/Shipments/ShipmentsController.php
+++ app/Http/Controllers/Customers/Shipments/ShipmentsController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers\Customers\Shipments;
 
 use App\Http\Controllers\Controller;
 use App\Models\Shipment;
+use App\Models\ShipmentTrackingNumber;
 use App\Models\UserAddressbook;
 use Illuminate\Http\Request;
 
@@ -44,35 +45,36 @@ class ShipmentsController extends Controller {
                 $this->createNewShipment($shipment, $user, $input);
             }
         });
+
+        return redirect()->back();
     }
 
     private function createNewShipment($shipment, $user, $input)
     {
         \DB::transaction(function() use ($shipment, $user, $input) {
             $bookingData = [
-                'source_id'         => 2,
-                'user_id'           => $user['id'],
-                'from'              => $input['from'],
-                'to'                => $input['to'],
-                'item_description'  => $input['item_description'],
-                'number_of_items'   => $shipment['number_of_items'],
-                'type_of_items'     => $shipment['type_of_items'],
-                'service_type'      => $shipment['service_type'],
-                'is_international'              => $shipment['is_international'],
+                'source_id'                     => 2,
+                'user_id'                       => $user['id'],
+                'from'                          => $input['from'],
+                'to'                            => $input['to'],
+                'item_description'              => $shipment['item_description'],
+                'number_of_items'               => $shipment['number_of_items'],
+                'service_type'                  => $shipment['service_type'] ? $shipment['service_type'] : 'metro_manila',
+                'is_international'              => $shipment['is_international'] ? $shipment['is_international'] : 'postal',
                 'collect_and_deposit'           => $shipment['is_cod'],
                 'collect_and_deposit_amount'    => $shipment['cod_amount'],
                 'account_name'                  => $shipment['account_name'],
                 'account_number'                => $shipment['account_number'],
                 'bank'                          => $shipment['bank'],
-                'charge_to'                     => $shipment['charge_to'],
-                'package_type'                  => $shipment['package_type'],
+                'charge_to'                     => $shipment['charge_to'] ? $shipment['charge_to'] : 'sender',
+                'package_type'                  => $shipment['package_type'] ? $shipment['package_type'] : 'small',
                 'insurance_declared_value'      => $shipment['avail_insurance'],
                 'insurance_amount'              => $shipment['insurance_amount'],
-                'length'            => $shipment['length'],
-                'width'             => $shipment['width'],
-                'height'            => $shipment['height'],
-                'weight'            => $shipment['weight'],
-                'status'            => 'pending'
+                'length'                        => $shipment['length'],
+                'width'                         => $shipment['width'],
+                'height'                        => $shipment['height'],
+                'weight'                        => $shipment['weight'],
+                'status'                        => 'pending'
             ];
 
             $shipment = Shipment::create($bookingData);
@@ -81,6 +83,25 @@ class ShipmentsController extends Controller {
                 'remarks'   => $shipment['remarks']
             ]);
 
+            // Create Tracking Number
+            ShipmentTrackingNumber::create([
+                'tracking_number'   => $this->createTrackingNumber(),
+                'shipment_id'       =>$shipment->id
+            ]);
         });
+    }
+
+    // Create unique tracking number
+    private function createTrackingNumber()
+    {
+        $trackingNumber = uniqid();
+        $check = ShipmentTrackingNumber::where('tracking_number', $trackingNumber)
+            ->first();
+
+        if ($check) {
+            $this->createTrackingNumber();
+        }
+
+        return $trackingNumber;
     }
 }
