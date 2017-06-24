3289407ec755765c7b6f4ac2d0c5f02cffef304f
Alleo Indong <freedom_war550@live.com.ph>
2017-06-25 02:38:15 +0800

    added cod page for customer

--- app/Http/Controllers/Customers/Shipments/CodShipmentsController.php
+++ app/Http/Controllers/Customers/Shipments/CodShipmentsController.php
@@ -2,10 +2,26 @@
 namespace App\Http\Controllers\Customers\Shipments;
 
 use App\Http\Controllers\Controller;
+use App\Models\Shipment;
 
 class CodShipmentsController extends Controller {
     public function index()
     {
-        return view('customers.shipments.cod.index');
+        $shipments = Shipment::with('cod')->has('cod')
+            ->where('collect_and_deposit', 1)
+            ->where('status', '<>', 'pending')
+            ->get();
+
+        $amounts = [
+            'pending'   => 0,
+            'collected' => 0,
+            'deposited' => 0
+        ];
+
+        foreach ($shipments as $shipment) {
+            $amounts[$shipment->cod->status] += $shipment->cod->collect_and_deposit_amount;
+        }
+
+        return view('customers.shipments.cod.index', compact('shipments', 'amounts'));
     }
 }
