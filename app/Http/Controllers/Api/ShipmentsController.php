9b198656d93d8983d5ef1db14696b25bf0806a0d
Alleo Indong <freedom_war550@live.com.ph>
2017-08-14 11:12:46 +0800

    testing

--- app/Http/Controllers/Api/ShipmentsController.php
+++ app/Http/Controllers/Api/ShipmentsController.php
@@ -12,7 +12,7 @@ use Illuminate\Http\Request;
 class ShipmentsController extends Controller {
     public function show($shipmentId)
     {
-        $shipment = Shipment::with('returnLogs.user.profile', 'events.user.profile', 'senderAddress', 'address')
+        $shipment = Shipment::with('returnLogs.logs.user.profile', 'events.user.profile', 'senderAddress', 'address')
             ->where('id', $shipmentId)->first();
 
         return response()->json($shipment, 200);
